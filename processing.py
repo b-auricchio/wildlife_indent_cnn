d052d7ce00ba7ea08b55dd2adafa2c42b4def2dc
b-auricchio <ls20682@bristol.ac.uk>
2023-01-29 16:23:18 +0000

    converted main.py to notebook

--- processing.py
+++ processing.py
@@ -1,31 +1,52 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-import csv
+import torch
+import time
 
 def to_dataframe(history):
     output = pd.DataFrame(
-    {'Accuracy': history["acc"],
-     'Train Loss': history["train_loss"],
-     'Val Loss': history["val_loss"],
-     'Mean LR': history["lr"]
+    {'accuracy': history["acc"],
+     'train_loss': history["train_loss"],
+     'val_loss': history["val_loss"],
+     'mean_lr': history["lr"]
     })
 
     return output
 
+def export(history, model, time_elapsed):
+    history = to_dataframe(history)
+    history["time_elapsed"] = [time_elapsed]
 
-def plot_acc(history):
-    plt.plot(history["acc"],"-x")
+    filename = model.name + '_' + time.strftime("%Y%m%d_%H%M%S")
+
+    history.to_csv((filename+'.csv'), encoding='utf-8', index=False)
+    torch.save(model.state_dict(), './dict/'+ filename +'.pt')
+
+def plot_history(history):
+    acc = history['accuracy'].to_list()
+    train_loss = history['train_loss'].to_list()
+    val_loss = history['val_loss'].to_list()
+    mean_lr = history['mean_lr'].to_list()
+
+    fig = plt.figure(figsize=(13, 4))
+
+    plt.subplot(1,3,1)
+    plt.plot(acc,"-x")
     plt.xlabel("Epoch")
     plt.ylabel("Accuracy")
 
-def plot_loss(history):
-    plt.plot(history["train_loss"], "-bx")
-    plt.plot(history["val_loss"],"-rx")
+    plt.subplot(1,3,2)
+    plt.plot(train_loss, "-bx")
+    plt.plot(val_loss,"-rx")
     plt.xlabel("Epoch")
     plt.ylabel("Loss")
     plt.legend(["train loss","val loss"])
-    
-def plot_lrs(history):
-    plt.plot(history["lr"])
+
+    plt.subplot(1,3,3)
+    plt.plot(mean_lr)
     plt.xlabel("Batch number")
     plt.ylabel("Learning rate")
+
+    plt.show()
+
+
