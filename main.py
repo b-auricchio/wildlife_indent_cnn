cfd7d7a3cbfbf79051411e5d1ebfcdc3b657ee9e
b-auricchio <ls20682@bristol.ac.uk>
2023-02-17 13:33:43 +0000

    Gradient accumulation implemented

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import torch
 import torch.optim as optim
 import torch.nn as nn
 from torch.utils.data import DataLoader
+from torchvision.datasets import Flowers102
 import time
 from IPython.display import display
 
-from inaturalist import BinaryBirds, train_transform, test_transform
+from datasets.inaturalist import Birds, train_transform, test_transform
 from functions import fit
 from functions import ToDeviceLoader
 from models.ResNet import ResNet, ResBlock, ResBottleneckBlock
@@ -14,17 +15,18 @@ import processing
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print("Using {} device".format(device))
 
-batch_size = 128
+batch_size = 32
+accum_iter = 4
 epochs = 90
 
-train_data = BinaryBirds(
-    split='train',
-    transform=train_transform
+train_data = Birds(
+    split = 'train',
+    transform = train_transform
 )
 
-test_data = BinaryBirds(
-    split='val',
-    transform=test_transform
+test_data = Birds(
+    split = 'val',
+    transform = test_transform
 )
 
 train_dl = DataLoader(train_data, batch_size=batch_size,shuffle=True)
@@ -33,7 +35,7 @@ test_dl = DataLoader(test_data, batch_size=batch_size)
 train_dl = ToDeviceLoader(train_dl,device)
 test_dl = ToDeviceLoader(test_dl,device)
 
-model = ResNet(3, ResBlock, [3,4,6,3], useBottleneck=False, outputs=126).to(device)
+model = ResNet(3, ResBottleneckBlock, [3,4,6,3], useBottleneck=True, outputs=126).to(device)
 
 max_lr=0.01
 grad_clip = 0.1
@@ -45,7 +47,7 @@ scheduler = torch.optim.lr_scheduler.OneCycleLR(optimiser, max_lr=max_lr, steps_
 
 start_time = time.time()
 
-history = fit(batch_size,epochs,train_dl,test_dl,model,loss_fn,optimiser,grad_clip=grad_clip, scheduler=scheduler)
+history = fit(batch_size,epochs,train_dl,test_dl,model,loss_fn,optimiser,accum_iter,grad_clip=grad_clip, scheduler=scheduler)
 
 print(f"--- {(time.time() - start_time):>0.1f} seconds ---")
 
