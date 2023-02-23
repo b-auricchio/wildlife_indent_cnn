resnet width scaling bug fixed

models/resnet.py
@@ -62,18 +62,20 @@ class ResNet(nn.Module):
         super().__init__()
         self.name = 'resnet'
 
+        layer0_features = int(64*k)
+
         self.layer0 = nn.Sequential(
-            nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3),
+            nn.Conv2d(in_channels, layer0_features, kernel_size=7, stride=2, padding=3),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(64),
+            nn.BatchNorm2d(layer0_features),
             nn.ReLU()
         )
 
         if filters == None:
             if useBottleneck:
-                filters = [64*k, 256*k, 512*k, 1024*k, 2048*k]
+                filters = [int(64*k), int(256*k), int(512*k), int(1024*k), int(2048*k)]
             else:
-                filters = [64*k, 64*k, 128*k, 256*k, 512*k]
+                filters = [int(64*k), int(64*k), int(128*k), int(256*k), int(512*k)]
 
         self.layer1 = nn.Sequential()
         self.layer1.add_module('conv2_1', resblock(filters[0], filters[1], downsample=False))

train.py
@@ -23,7 +23,7 @@ else: epochs = cfg.epochs
 
 dataset = eval(cfg.dataset)
 
-batch_size = dataset.batch_size
+batch_size = cfg.batch_size
 num_classes = dataset.num_known_classes
 datasets = dataset.get_datasets(cfg.download)
 
@@ -39,7 +39,7 @@ grad_clip = cfg.grad_clip
 weight_decay = cfg.weight_decay
 label_smoothing = dataset.label_smoothing
 
-loss_fn = nn.CrossEntropyLoss(label_smoothing)
+loss_fn = nn.CrossEntropyLoss(label_smoothing=label_smoothing)
 optimiser = optim.Adam(model.parameters(),weight_decay=weight_decay,lr=max_lr)
 scheduler = utils.get_scheduler(optimiser, cfg)
 
@@ -47,9 +47,10 @@ print("\nTraining \n ----------------------")
 print(f"Model: {cfg.model}")
 print(f"Dataset: {cfg.dataset}")
 print("\nHyperparameters \n ----------------------")
-print(f"ETA: {max_lr}")
+print(f"Eta: {max_lr}")
 print(f"Batch size: {batch_size}")
 print(f"Image size: {dataset.image_size}")
+print(f"Width scaling: {cfg.width_scaling}")
 print("\n")
 
 start_time = time.time()

utils.py
@@ -95,7 +95,7 @@ def to_device(data,device):
 def get_scheduler(optim, cfg, steps_per_epoch=None):
     if cfg.scheduler == 'cosine':
         num_restarts = cfg.num_restarts
-        return torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optim, T_0=cfg.epochs/(num_restarts), eta_min=cfg.eta*0.001)
+        return torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optim, T_0=int(cfg.epochs/(num_restarts)), eta_min=cfg.eta*0.001)
 
     if cfg.scheduler == 'onecycle':
         scheduler = torch.optim.lr_scheduler.OneCycleLR(optim, max_lr=cfg.eta, steps_per_epoch=steps_per_epoch, epochs=cfg.epochs)